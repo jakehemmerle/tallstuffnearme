4aaf62c53e5aca9a8fd372c2e6bbf62853cb3e0c
Jake Hemmerle <jakehemmerle@protonmail.com>
2021-12-21 14:33:05 -0500

    parsing and slicing of dat files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ~~wherecanibasejump~~ tallstuffnearme
+# tallstuffnearme
 
 This repo should show tall stuff around you based on the [FAA Digital Obstacle File](https://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/dof/)
 
@@ -6,13 +6,22 @@ This repo should show tall stuff around you based on the [FAA Digital Obstacle F
 
 decompress zip file in `faa-data`
 
-then run `yarn` and `yarn start`
+then run `yarn` (to install deps) and `yarn start` to run program
 
-----
+## Devs
 
-FAA Obsticle List (contains antennas, buildings, power towers, stacks, tanks, windmills, EVERYTHING): 
-Earth: Download USGS maps here: https://apps.nationalmap.gov/downloader/#/
+[FAA Digital Obstacle File](https://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/dof/)
+[FAA Obstacle Data FAQ](https://www.faa.gov/air_traffic/flight_info/aeronav/obst_data/)
 
-## Dev Docs
+For parsing earth data down the road(?):
+[USGS map](https://apps.nationalmap.gov/downloader/#/)
 
-Google Maps JS API: https://developers.google.com/maps/documentation/javascript/tutorials
+Google Maps JS API: <https://developers.google.com/maps/documentation/javascript/tutorials>
+
+## Legal
+
+From the DOF FAQ:
+
+Q: Is the DOF data considered to be in the public domain? Are there any restrictions on using the DOF data?
+
+A: Yes, it is in the public domain. There are no restrictions on how you may use the data, but you are not allowed to change the data.
--- src/parseDAT.ts
+++ src/parseDAT.ts
@@ -1,11 +1,81 @@
-import { readFile } from 'fs';
+import { readFileSync } from 'fs';
 
-export const parseOhioAsText = () => {
-    readFile('./faa-data/39-OH.Dat', { encoding: 'utf8' }, (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log(data);
-    })
+// interface FAAObject {
+//     OASNumber: number;
+//     Verified: boolean;
+//     Country: string;
+//     State: string;
+//     City: string;
+//     Latitude: string;
+//     Longitude: string;
+//     ObstacleType: string;
+//     AGL: number;
+//     AMSL: number;
+//     H: number;
+//     AccV: string;
+//     MarInd: string;
+//     FAAStudyNumber: string;
+//     Action: string;
+//     JDate: string;
+// }
+
+interface FAAObject {
+    OASNumber: string;
+    Verified: string;
+    Country: string;
+    State: string;
+    City: string;
+    Latitude: string;
+    Longitude: string;
+    ObstacleType: string;
+    AGL: string;
+    AMSL: string;
+    LT: string;
+    H: string;
+    AccV: string;
+    MarInd: string;
+    FAAStudyNumber: string;
+    Action: string;
+    JDate: string;
 }
+
+const rawStringToFAAObject = (line: string): FAAObject => {
+
+    const currentObject: FAAObject = {
+        OASNumber: line.slice(0, 10).trimEnd(),
+        Verified: line.slice(10, 11),
+        Country: line.slice(12, 14),
+        State: line.slice(15, 17),
+        City: line.slice(18, 34).trimEnd(),
+        Latitude: line.slice(35, 47),
+        Longitude: line.slice(48, 61),
+        ObstacleType: line.slice(62, 80).trimEnd(),
+        AGL: line.slice(83, 88),
+        AMSL: line.slice(89, 94),
+        LT: line.slice(95, 96),
+        H: line.slice(97, 98),
+        AccV: line.slice(99, 100),
+        MarInd: line.slice(101, 102),
+        FAAStudyNumber: line.slice(103, 117),
+        Action: line.slice(118, 119),
+        JDate: line.slice(120, 127)
+    };
+
+    return currentObject;
+
+}
+
+export const parseOhioAsText = (): FAAObject[] => {
+    let parsedLocations: FAAObject[] = [];
+    const rawText = readFileSync('./faa-data/39-OH.Dat', { encoding: 'utf8' });
+    const rawLines = rawText.split(/\r?\n/);
+
+    const locationsAsRawStrings = rawLines.slice(4); // remove first few lines of non-object shit
+
+    locationsAsRawStrings.forEach((line) => {
+        parsedLocations.push(rawStringToFAAObject(line));
+    })
+    
+    return parsedLocations;
+}
+
