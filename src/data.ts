1f158edd27aed79f6d729c8753eee544ace6dfec
Jake Hemmerle <jakehemmerle@protonmail.com>
2021-12-25 22:45:44 -0600

    refactored functions

--- src/data.ts
+++ src/data.ts
@@ -17,64 +17,54 @@ places	degrees	distance
 
 */
 
-// lat and long are the string as it's formatted in the DB
+// input format eg. ' 40 06 17.00N'
+const DMSStringToDD = (dms: string): number => {
+    // parse strings
+    const decimal = parseFloat(dms.slice(0, 3));
+    const minute = parseFloat(dms.slice(4, 6));
+    const second = parseFloat(dms.slice(7, 12));
+    const direction = dms.slice(12, 13);
 
-// def dms2dd(degrees, minutes, seconds, direction):
-//     dd = float(degrees) + float(minutes)/60 + float(seconds)/(60*60);
-//     if direction == 'W' or direction == 'S':
-//         dd *= -1
-//     return dd;
-export const DMSToDD = (lat: string, long: string) => {
+    // convert to decimal
+    let dd = decimal + (minute / 60) + (second / (60 * 60));
 
+    // positive or negative dd based on direction of dms
+    if (direction === 'W' || direction === 'S') {
+        dd *= -1;
+    }
+
+    return dd;
 }
 
+const rawStringToFAAObject = (line: string): Prisma.FAAObjectCreateInput => {
+
+    
+
+    const currentObject: Prisma.FAAObjectCreateInput = {
+        OASNumber: parseInt(line.slice(0, 2).concat(line.slice(3, 10)).trimEnd()),
+        Verified: line.slice(10, 11),
+        Country: line.slice(12, 14),
+        State: line.slice(15, 17),
+        City: line.slice(18, 34).trimEnd(),
+        Latitude: DMSStringToDD(line.slice(34, 47)),
+        Longitude: DMSStringToDD(line.slice(48, 61)),
+        ObstacleType: line.slice(62, 80).trimEnd(),
+        AGL: parseInt(line.slice(83, 88)),
+        AMSL: parseInt(line.slice(89, 94)),
+        LT: line.slice(95, 96),
+        H: line.slice(97, 98),
+        AccV: line.slice(99, 100),
+        MarInd: line.slice(101, 102),
+        FAAStudyNumber: line.slice(103, 117),
+        Action: line.slice(118, 119),
+        JDate: line.slice(120, 127)
+    };
+
+    return currentObject;
+}
 
 export const insertDatFileIntoDB = async (path: string): Promise<FAAObject[]> => {
     const prisma = new PrismaClient();
-
-    const rawStringToFAAObject = (line: string): Prisma.FAAObjectCreateInput => {
-
-        // input format is: '040 06 17.00N'
-        const DMSStringToDD = (dms: string): number => {
-            // parse strings
-            const decimal = parseFloat(dms.slice(0, 3));
-            const minute = parseFloat(dms.slice(4, 6));
-            const second = parseFloat(dms.slice(7, 12));
-            const direction = dms.slice(12, 13);
-
-            // convert to decimal
-            let dd = decimal + (minute / 60) + (second / (60 * 60));
-
-            // positive or negative dd based on direction of dms
-            if (direction === 'W' || direction === 'S') {
-                dd *= -1;
-            }
-
-            return dd;
-        }
-
-        const currentObject: Prisma.FAAObjectCreateInput = {
-            OASNumber: parseInt(line.slice(0, 2).concat(line.slice(3, 10)).trimEnd()),
-            Verified: line.slice(10, 11),
-            Country: line.slice(12, 14),
-            State: line.slice(15, 17),
-            City: line.slice(18, 34).trimEnd(),
-            Latitude: DMSStringToDD(line.slice(34, 47)),
-            Longitude: DMSStringToDD(line.slice(48, 61)),
-            ObstacleType: line.slice(62, 80).trimEnd(),
-            AGL: parseInt(line.slice(83, 88)),
-            AMSL: parseInt(line.slice(89, 94)),
-            LT: line.slice(95, 96),
-            H: line.slice(97, 98),
-            AccV: line.slice(99, 100),
-            MarInd: line.slice(101, 102),
-            FAAStudyNumber: line.slice(103, 117),
-            Action: line.slice(118, 119),
-            JDate: line.slice(120, 127)
-        };
-    
-        return currentObject;
-    }
 
     // parse raw text
     const rawText: string = readFileSync(path, { encoding: 'utf8' });
