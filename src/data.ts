b0d23dd9d741256382c3700539a9048c82a643fd
Jake Hemmerle <jakehemmerle@protonmail.com>
2021-12-25 21:24:21 -0600

    index now populates DB with ohkyin data!

---
+++ src/data.ts
@@ -0,0 +1,72 @@
+import { readFileSync } from 'fs';
+import { PrismaClient, Prisma, FAAObject } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+/*
+decimal degrees to distance
+
+places	degrees	distance
+0	1.0	111 km    ~ 111,319.9km
+1	0.1	11.1 km
+2	0.01	1.11 km
+3	0.001	111 m
+4	0.0001	11.1 m
+5	0.00001	1.11 m
+6	0.000001	0.111 m
+7	0.0000001	1.11 cm
+8	0.00000001	1.11 mm
+
+*/
+
+
+
+
+
+export const insertDatFileIntoDB = async (path: string): Promise<FAAObject[]> => {
+
+    const rawStringToFAAObject = (line: string): Prisma.FAAObjectCreateInput => {
+
+        const currentObject: Prisma.FAAObjectCreateInput = {
+            OASNumber: parseInt(line.slice(0, 2).concat(line.slice(3, 10)).trimEnd()),
+            Verified: line.slice(10, 11),
+            Country: line.slice(12, 14),
+            State: line.slice(15, 17),
+            City: line.slice(18, 34).trimEnd(),
+            Latitude: line.slice(35, 47),
+            Longitude: line.slice(48, 61),
+            ObstacleType: line.slice(62, 80).trimEnd(),
+            AGL: parseInt(line.slice(83, 88)),
+            AMSL: parseInt(line.slice(89, 94)),
+            LT: line.slice(95, 96),
+            H: line.slice(97, 98),
+            AccV: line.slice(99, 100),
+            MarInd: line.slice(101, 102),
+            FAAStudyNumber: line.slice(103, 117),
+            Action: line.slice(118, 119),
+            JDate: line.slice(120, 127)
+        };
+    
+        return currentObject;
+    }
+
+    // parse raw text
+    const rawText: string = readFileSync(path, { encoding: 'utf8' });
+    const rawLines: string[] = rawText.split(/\r?\n/);
+
+    // raw text to json
+    const cleanedStrings: string[] = rawLines.slice(4, rawLines.length - 1); // remove first few lines of non-object shit
+    const insertableObjects: Prisma.FAAObjectCreateInput[] = cleanedStrings.map((rawLocation) => rawStringToFAAObject(rawLocation));
+
+    // json into db
+    console.log(`adding ${insertableObjects.length} objects to db...`);
+
+    return Promise.all(
+        insertableObjects.map(async (object) => {
+            return prisma.fAAObject.create({
+                data: object,
+            })
+        })
+    )
+    
+}
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import { PrismaClient, Prisma } from '@prisma/client'
-import { insertDatFileIntoDB } from './parseDAT';
+import { insertDatFileIntoDB } from './data';
 
 const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
-async function main() {
-  const objects = await insertDatFileIntoDB("./faa-data/39-OH.Dat");
-  console.log("complete ohio!");
-
+async function populateDB() {
+  await insertDatFileIntoDB("./faa-data/39-OH.Dat");
+  console.log("complete oh!");
+  await insertDatFileIntoDB("./faa-data/21-KY.Dat");
+  console.log("complete ky!");
+  await insertDatFileIntoDB("./faa-data/18-IN.Dat");
+  console.log("complete in!");
 }
 
-main()
+populateDB()
   .catch((e) => {
     throw e
   })
