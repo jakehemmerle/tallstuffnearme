43d82ae3246430cc31ea3d65189f63aeb9980e1a
Jake Hemmerle <jakehemmerle@protonmail.com>
2021-12-26 00:10:06 -0600

    updated tests

--- src/data.test.ts
+++ src/data.test.ts
@@ -1,15 +1,14 @@
 import { getQueryCoordinates, DD, LocationAndRadius,  } from './data';
 
 test('run getQueryCoordinates with sample values', () => {
-    console.log(`samples long lat: 0, 0`);
-    console.log(`samples radius: 10 miles`);
+    // lat long at 0, 0
+    // radius at 10 miles
 
-    const location = {
+    const location: DD = {
         lattitude: 0,
         longitude: 0,
     };
 
     const queryCoordinates: LocationAndRadius = getQueryCoordinates(location, 10);
     console.log(queryCoordinates);
-
 });
