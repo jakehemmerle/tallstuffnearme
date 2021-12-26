a64d8170feb17b6db3d6710403073e017560ecaf
Jake Hemmerle <jakehemmerle@protonmail.com>
2021-12-26 13:27:53 -0600

    yargs works somehow

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,10 +1,14 @@
 import yargs from 'yargs/yargs';
 
-const argv = yargs(process.argv.slice(2)).options({
-  a: { type: 'boolean', default: false },
-  b: { type: 'string', demandOption: true },
-  c: { type: 'number', alias: 'chill' },
-  d: { type: 'array' },
-  e: { type: 'count' },
-  f: { choices: ['1', '2', '3'] }
-}).argv;
+var argv = yargs(process.argv.slice(2))
+    .usage('Usage: $0 <command> [options]')
+    .command('count', 'Count the lines in a file')
+    .example('$0 count -f foo.js', 'count the lines in the given file')
+    .alias('f', 'file')
+    .nargs('f', 1)
+    .describe('f', 'Load a file')
+    .demandOption(['f'])
+    .help('h')
+    .alias('h', 'help')
+    .epilog('copyright 2019')
+    .argv;
