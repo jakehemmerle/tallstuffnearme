687132d5755331ee313bac85b6d18dde96c41a06
Jake Hemmerle <jakehemmerle@protonmail.com>
2022-08-18 03:59:34 -0400

    did some work on csv generator for frontend

---
+++ bin/csv.ts
@@ -0,0 +1,55 @@
+import { stringify } from 'csv-stringify';
+import assert from 'assert';
+import { searchObjects, defaultQuery } from '../src/controller';
+
+const data = [];
+const inputData = searchObjects(defaultQuery);
+
+// Initialize the stringifier
+const stringifier = stringify({
+  delimiter: ','
+});
+// Use the readable stream api to consume CSV data
+stringifier.on('readable', function () {
+  let row;
+  while ((row = stringifier.read()) !== null) {
+    data.push(row);
+  }
+});
+// Catch any error
+stringifier.on('error', function (err) {
+  console.error(err.message);
+});
+// When finished, validate the CSV output with the expected value
+stringifier.on('finish', function () {
+  const newData = data.join('');
+  console.log(newData);
+});
+// Write records to the stream
+
+
+inputData.then(data => {
+  stringifier.write([
+    'Latitude',
+    'Longitude',
+    'AGL',
+    'ObjectType',
+    'OASNumber'
+  ]);
+  data.forEach(row => {
+    let someData = [
+      row.FAAObject.Latitude,
+      row.FAAObject.Longitude,
+      row.FAAObject.AGL,
+      row.FAAObject.ObjectType,
+      row.FAAObject.OASNumber
+    ];
+    // console.log(someData);
+    stringifier.write(someData);
+  })
+}).then(() => {
+  stringifier.end();
+});
+// stringifier.write(['someone', 'x', '1022', '1022', '', '/home/someone', '/bin/bash']);
+// Close the writable stream
+// stringifier.end();
